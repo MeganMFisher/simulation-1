9a8f0232d1d9c0e52dd98f9a30f2226f6c7c31d9
MeganMFisher <meganmichelle318@gmail.com>
2017-08-20 11:28:29 -0600

    styling homepage

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import router from './router';
-// import './reset.css';
-// import './App.css';
+import './reset.css';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div >
-          <h2>Simulation 1</h2>
+      <div>
           { router }
       </div>
     );
