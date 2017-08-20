2ac5ecf3bf4b45616593016dceee077ddbf30bf4
MeganMFisher <meganmichelle318@gmail.com>
2017-08-20 10:30:10 -0600

    troubleshooting

--- app/server/server.js
+++ app/server/server.js
@@ -4,17 +4,16 @@ const cors = require('cors');
 const massive = require('massive');
 const controller = require('./controller');
 const config = require('./config');
-const connectionString = "postgres://atxfgxzt:WOGAB3jMBwsoDhtug8pb7rXFI_ho2S7b@pellefant.db.elephantsql.com:5432/atxfgxzt";
 
 let app = express();
 app.use(bodyParser.json());
 app.use(cors());
-massive( connectionString ).then( dbInstance => app.set('db', dbInstance) );
-// massive(config.database).then(db => {
-//     app.set('db', db)
-// }).catch((err) => {
-//     console.log(err)
-// })
+
+massive(config.database).then(db => {
+    app.set('db', db)
+}).catch((err) => {
+    console.log(err)
+})
 
 
 app.get('/api/shelf/:id', controller.getShelves)
